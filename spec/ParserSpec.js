e0ebe364331875a53b7726a43f379df591a0626e
Martyn Wilkinson <github@bigmartyn.com>
2012-11-18 22:01:00 +0000

    Refactor parser into state machine to better handle knotty edge cases.

--- spec/ParserSpec.js
+++ spec/ParserSpec.js
@@ -1,43 +1,71 @@
 describe("Parser", function() {
-  var parser;
+    var parser;
+    var timer;
 
-  beforeEach(function() {
-    parser = new CurrencyParser();
-  });
+    beforeEach(function () {
+        parser = new CurrencyParser();
+    });
 
-  it("should reject an empty string", function() {
-	  expect(function() { parser.parse("");          }).toThrow(new Error("empty string"));
-	  expect(function() { parser.parse("         "); }).toThrow(new Error("empty string"));
-  });
+    it("should reject an empty string", function() {
+        expect(function() { parser.parse("");          }).toThrow(new Error("empty string"));
+        expect(function() { parser.parse("         "); }).toThrow(new Error("empty string"));
+    });
 
-  it("should reject a non-numeric character", function() {
-	  expect(function() { parser.parse("1x3"); }).toThrow(new Error("non-numeric character"));
-  });
+    it("should reject a non-numeric character", function() {
+        expect(function() { parser.parse("1x"); }).toThrow(new Error("non-numeric character"));
+        expect(function() { parser.parse("£1x.0p"); }).toThrow(new Error("non-numeric character"));
+    });
 
-  it("should accept an single-digit string", function() {
-	  expect(parser.parse("4")).toEqual(4);
-  });
+    it("should accept an single-digit string", function() {
+        expect(parser.parse("4")).toEqual(4);
+    });
 
-  it("should accept an double-digit string", function() {
-	  expect(parser.parse("85")).toEqual(85);
-  });
+    it("should accept an double-digit string", function() {
+        expect(parser.parse("85")).toEqual(85);
+    });
 
-  it("should accept digits and a trailing pence symbol", function() {
-	  expect(parser.parse("197p")).toEqual(197);
-	  expect(parser.parse("2p")).toEqual(2);	  
-  });
+    it("should accept one or more digits and a trailing pence symbol", function() {
+        expect(parser.parse("197p")).toEqual(197);
+        expect(parser.parse("2p")).toEqual(2);      
+    });
 
-  it("should accept a bare number with pounds and pence", function() {
-	  expect(parser.parse("1.87")).toEqual(187);
-  });
+    it("should accept a bare number with hundreds and units", function() {
+        expect(parser.parse("1.87")).toEqual(187);
+    });
 
-  it("should reject numbers with too many unit separators", function() {
-	  expect(function() { parser.parse("1.8.7"); }).toThrow(new Error("non-numeric character"));	  
-  });
+    it("should accept a number with a leading currency symbol", function() {
+        expect(parser.parse("£1.23")).toEqual(123);
+    });
 
-  it("should ignore leading zeros", function() {
-	  expect(parser.parse("000013")).toEqual(13);
-	  expect(parser.parse("001.41p")).toEqual(141);
-  });
+    it("should accept a number with a leading currency symbol", function() {
+        expect(parser.parse("£1.23")).toEqual(123);
+        expect(parser.parse("£2")).toEqual(200);
+        expect(parser.parse("£10")).toEqual(1000);
+    });
+
+    it("should accept anumber with leading and trailing symbols", function() {
+        expect(parser.parse("£1.87p")).toEqual(187);
+        expect(parser.parse("£1p")).toEqual(100);
+        expect(parser.parse("£1.p")).toEqual(100);
+    });
+
+    it("should reject numbers with no digits", function() {
+        expect(function() { parser.parse("p"); }).toThrow(new Error("missing digits"));      
+        expect(function() { parser.parse(".p"); }).toThrow(new Error("missing digits"));      
+        expect(function() { parser.parse("£p"); }).toThrow(new Error("missing digits"));      
+        expect(function() { parser.parse("£.p"); }).toThrow(new Error("missing digits"));      
+    });
+
+    it("should reject numbers with too many unit separators", function() {
+        expect(function() { parser.parse("1.8.7"); }).toThrow(new Error("non-numeric character"));      
+    });
+
+    it("should ignore leading zeros", function() {
+        expect(parser.parse("000013")).toEqual(13);
+        expect(parser.parse("000013p")).toEqual(13);
+        expect(parser.parse("001.41p")).toEqual(141);
+        expect(parser.parse("£001.41")).toEqual(141);
+        expect(parser.parse("£001.41p")).toEqual(141);
+    });
 
 });
--- src/CurrencyParser.js
+++ src/CurrencyParser.js
@@ -2,43 +2,130 @@
  */
 
 function CurrencyParser() {
-	var unitSeparator = '.',
-		unitsSuffix = 'p';
+	var currencySymbol = '£',
+		unitsSeparator = '.',
+		currencySuffix = 'p';
 
 	this.parse = function(currencyString) {
-		var hundreds, i, parsed, result, units;
+		var hundreds, i, nextState, result, state, units;
 
 		function isDigit(c) {
 			return ('0' <= c && c <= '9');
 		}
 
 		currencyString = currencyString.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-		if (currencyString == "") {
-			throw new Error("empty string");
-		}
 
 		hundreds = null;
-		units = 0;
-		for (i = 0, parsed = false; !parsed && i < currencyString.length; ++i) {
-			if (isDigit(currencyString.charAt(i))) {
-				units = (units * 10) + parseInt(currencyString.charAt(i), 10);
-			} else if (currencyString.indexOf(unitsSuffix, i) == i) {
-				parsed = true;
-			} else if (currencyString.indexOf(unitSeparator, i) == i) {
-				if (hundreds != null) {
-					throw new Error("non-numeric character");
+		units = null;
+
+		for (state = "checkForEmptyString"; state != "done"; state = nextState) {
+			switch (state) {
+			case 'checkForEmptyString':
+				if (0 == currencyString.length) {
+					nextState = "errorEmptyString";
+				} else {
+					i = 0;
+					nextState = "lookingForCurrencyPrefix";
 				}
-				hundreds = units;
-				units = 0;
-			} else {
+				break;
+			case "lookingForCurrencyPrefix":
+				if (currencyString.indexOf(currencySymbol, i) !== i) {
+					nextState = "parsePotentialUnits";
+				} else {
+					i += currencySymbol.length;
+					nextState = "parsingHundredsDigits";
+				}
+				break;
+			case "parsingHundredsDigits":
+				if (i == currencyString.length) {
+					nextState = 'checkDigitsPresent';
+				} else if (!isDigit(currencyString.charAt(i))) {
+					nextState = "lookingForUnitsSeparator";
+				} else {
+					hundreds = (null === hundreds) ? 0 : (hundreds * 10);
+					hundreds += parseInt(currencyString.charAt(i), 10);
+					++i;
+					nextState = "parsingHundredsDigits";
+				}
+				break;
+			case "lookingForUnitsSeparator":
+				if (currencyString.indexOf(unitsSeparator, i) != i) {
+					 nextState = "lookingForcurrencySuffix";
+				} else {
+					i += unitsSeparator.length;
+					nextState = "parseUnitsDigits";
+				}
+				break;
+			case "parsePotentialUnits":
+				if (i == currencyString.length) {
+					nextState = 'checkDigitsPresent';
+				} else if (currencyString.indexOf(unitsSeparator, i) == i) {
+					nextState = "bumpUnitsToHundreds";
+				} else if (!isDigit(currencyString.charAt(i))) {
+					nextState = "lookingForcurrencySuffix";
+				} else {
+					units = (null === units) ? 0 : (units * 10);
+					units += parseInt(currencyString.charAt(i), 10);
+					++i;
+					nextState = "parsePotentialUnits";
+				}
+				break;
+			case 'bumpUnitsToHundreds':
+				if (null !== hundreds) {
+					nextState = "errorNonNumericCharacter";
+				} else {
+					hundreds = units;
+					units = null;
+					++i;
+					nextState = "parseUnitsDigits"
+				}
+				break;
+			case "parseUnitsDigits":
+				if (i == currencyString.length) {
+					nextState = 'checkDigitsPresent';
+				} else if (!isDigit(currencyString.charAt(i))) {
+					nextState = "lookingForcurrencySuffix";
+				} else {
+					units = (null === units) ? 0 : (units * 10);
+					units += parseInt(currencyString.charAt(i), 10);
+					++i;
+					nextState = "parseUnitsDigits";
+				}
+				break;
+			case "lookingForcurrencySuffix":
+				if (currencyString.indexOf(currencySuffix, i) != i) {
+					 nextState = "errorNonNumericCharacter";
+				 } else {
+					 nextState = "checkDigitsPresent";
+				 }
+				break;
+			case "checkDigitsPresent":
+				if (null === hundreds && null === units) {
+					nextState = "errorMissingDigits";
+				} else {
+					nextState = "calculateResult";
+				}
+				break;
+			case "calculateResult":
+				hundreds = (null === hundreds) ? 0 : hundreds;
+				units = (null === units) ? 0 : units;
+				result = (hundreds * 100) + units;
+				nextState = "done";
+				break;
+			case "errorEmptyString":
+				throw new Error("empty string");
+				break;
+			case "errorNonNumericCharacter":
 				throw new Error("non-numeric character");
+				break;
+			case "errorMissingDigits":
+				throw new Error("missing digits");
+				break;
+			default:
+				throw new Error("internal error - unknown state: " + state);
+				break;
 			}
 		}
-		if (hundreds === null) {
-			result = units;
-		} else {
-			result = (hundreds * 100) + units;
-		}
 		return result;
-	} 
+    } 
 }
