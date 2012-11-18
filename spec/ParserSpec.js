1b938425cc9efbc7c90b3f8f3cdfd54b16b4dc45
Martyn Wilkinson <github@bigmartyn.com>
2012-11-18 19:31:02 +0000

    Added units digits parsing and handling of the optional units suffix.

--- spec/ParserSpec.js
+++ spec/ParserSpec.js
@@ -9,6 +9,32 @@ describe("Parser", function() {
 	  expect(function() {
 		  parser.parse("");
 	  	  }).toThrow(new Error("empty string"));
+	  expect(function() {
+		  parser.parse("         ");
+	  	  }).toThrow(new Error("empty string"));
+  });
+
+  it("should reject a non-numeric character", function() {
+	  expect(function() {
+		  parser.parse("1x3");
+	  	  }).toThrow(new Error("non-numeric character"));
+  });
+
+  it("should accept an single-digit string", function() {
+	  expect(parser.parse("4")).toEqual(4);
+  });
+
+  it("should accept an double-digit string", function() {
+	  expect(parser.parse("85")).toEqual(85);
+  });
+
+  it("should ignore leading zeros", function() {
+	  expect(parser.parse("000013")).toEqual(13);
+  });
+
+  it("should accept digits and a trailing pence symbol", function() {
+	  expect(parser.parse("197p")).toEqual(197);
+	  expect(parser.parse("2p")).toEqual(2);	  
   });
 
 });
--- src/CurrencyParser.js
+++ src/CurrencyParser.js
@@ -2,12 +2,30 @@
  */
 
 function CurrencyParser() {
-	this.parse = function(currencyString) {
-		var result = 0;
+	var unitsSuffix = 'p';
 
-		if (currencyString == "") { throw new Error("empty string");}
-		return result;
+	this.parse = function(currencyString) {
+		var i, parsed, units;
+		
+		function isDigit(c) {
+			return ('0' <= c && c <= '9');
+		}
+
+		currencyString = currencyString.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+		if (currencyString == "") {
+			throw new Error("empty string");
+		}
+
+		units = 0;
+		for (i = 0, parsed = false; !parsed && i < currencyString.length; ++i) {
+			if (isDigit(currencyString.charAt(i))) {
+				units = (units * 10) + parseInt(currencyString.charAt(i), 10);
+			} else if (currencyString.indexOf(unitsSuffix, i) == i) {
+				parsed = true;
+			} else {
+				throw new Error("non-numeric character");
+			}
+		}
+		return units;
 	} 
 }
-
- 
