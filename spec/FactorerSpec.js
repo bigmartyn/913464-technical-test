2f0c7f877a240a4d731125b26a280bd9f9a59b11
Martyn Wilkinson <github@bigmartyn.com>
2012-11-19 00:52:51 +0000

    Add denominations for USD for illustrative purposes and unit tests to check they work.

--- spec/FactorerSpec.js
+++ spec/FactorerSpec.js
@@ -10,4 +10,48 @@ describe("Factorer", function() {
         expect(factorer.factor(-1)).toEqual([]);
     });
 
+    it("should return exactly one of each of the denominations given that exact amount", function() {
+        expect(factorer.factor(1)).toEqual([[1, '1p']]);
+        expect(factorer.factor(2)).toEqual([[1, '2p']]);
+//        expect(factorer.factor(5)).toEqual([[1, '5p']]);
+//        expect(factorer.factor(10)).toEqual([[1, '10p']]);
+        expect(factorer.factor(20)).toEqual([[1, '20p']]);
+        expect(factorer.factor(50)).toEqual([[1, '50p']]);
+        expect(factorer.factor(100)).toEqual([[1, '&pound;1']]);
+        expect(factorer.factor(200)).toEqual([[1, '&pound;2']]);
+    });
+
+    it("should return 1x one pound, 1 x 20p, 1 x 2p and 1 x 1p for 123 pence", function() {
+        expect(factorer.factor(123)).toEqual([[1, '&pound;1'], [1, '20p'], [1, '2p'], [1, '1p']]);
+    });
+
+    it("should return 2 x two pound and 1 x one pound for five pounds", function() {
+        expect(factorer.factor(500)).toEqual([[2, '&pound;2'], [1, '&pound;1']]);
+    });
+    
+    it("should return 1 x 50p and 2 x 20p and 4 x 2p and 1 x 1p for 99 pence", function() {
+        expect(factorer.factor(99)).toEqual([[1, '50p'], [2, '20p'], [4, '2p'], [1, '1p']]);
+    });
+    
+    it("should be possible to set USD as the currency format and factor dollars", function() {
+        expect(factorer.setCurrency('USD')).toEqual(true);
+        expect(factorer.factor(1)).toEqual([[1, '1&cent;']]);
+        expect(factorer.factor(5)).toEqual([[1, '5&cent;']]);
+        expect(factorer.factor(10)).toEqual([[1, '10&cent;']]);
+        expect(factorer.factor(25)).toEqual([[1, '25&cent;']]);
+        expect(factorer.factor(50)).toEqual([[1, '50&cent;']]);
+        expect(factorer.factor(100)).toEqual([[1, '$1']]);
+        expect(factorer.factor(200)).toEqual([[1, '$2']]);
+        expect(factorer.factor(500)).toEqual([[1, '$5']]);
+        expect(factorer.factor(1000)).toEqual([[1, '$10']]);
+        expect(factorer.factor(2000)).toEqual([[1, '$20']]);
+        expect(factorer.factor(5000)).toEqual([[1, '$50']]);
+        expect(factorer.factor(10000)).toEqual([[1, '$100']]);
+        expect(factorer.factor(39999)).toEqual([[3, '$100'],[1, '$50'],[2, '$20'],[1, '$5'],[2, '$2'],[1, '50&cent;'],[1, '25&cent;'],[2, '10&cent;'],[4, '1&cent;']]);
+    });
+
+    it("should reject an attempt to set an unknown currency", function() {
+        expect(factorer.setCurrency("EUR")).toEqual(false);
+    });
+    
 });
--- src/CurrencyFactorer.js
+++ src/CurrencyFactorer.js
@@ -4,15 +4,21 @@
 function CurrencyFactorer() {
     var currentFormat = 'GBP';
     var denominations = {
-        'GBP': {"£2": 200, "£1": 100, "50p": 50, "20p": 20, "2p": 2, "1p": 1}
-    };
+        'GBP': {
+            // TODO: find out why 5p and 10p have been omitted
+            names:  ["&pound;2", "&pound;1", "50p", "20p", "2p", "1p"],
+            values: [      200,        100,    50,    20,    2,    1]},
+        'USD': {
+            names:  ["$100", "$50", "$20", "$10", "$5", "$2", "$1", "50&cent;", "25&cent;", "10&cent;", "5&cent;", "1&cent;"],
+            values: [10000,  5000,  2000,  1000,  500,  200,  100,   50,         25,         10,         5,         1]}
+        };
 
     /*  set currency code for factorer
      */
     this.setCurrency = function(currency) {
         var result = false;
 
-        if (formats.hasOwnProperty(currency)) {
+        if (denominations.hasOwnProperty(currency)) {
             currentFormat = currency;
             result = true;
         }
@@ -22,9 +28,20 @@ function CurrencyFactorer() {
     /*  return monetary denominations needed to make up amount
      */
     this.factor = function(amount) {
-        var result;
+        var coinage, i, nmrCoins, result, values;
 
         result = [];
+        if (amount > 0) {
+            coinage = denominations[currentFormat].names;
+            values = denominations[currentFormat].values;
+            for (i = 0; i < coinage.length; ++i) {
+                if (amount >= values[i]) {
+                    nmrCoins = Math.floor(amount / values[i]);
+                    amount -= nmrCoins * values[i];
+                    result.push([nmrCoins, coinage[i]]);
+                }
+            }
+        }
         return result;
         }
 
